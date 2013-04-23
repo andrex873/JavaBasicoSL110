8bf7d8b6e7c0e763da2e7b94e6e7dbb039651533
Andres Mendez <andrex873@gmail.com>
2013-04-22 20:18:03 -0500

    Se agrega lab 07 extra numeral 1

---
+++ exercises/07_loops/extra/ForElevatorTest.java
@@ -0,0 +1,8 @@
+public class ForElevatorTest{
+    public static void main(String args[]){
+        ForElevator e = new ForElevator();
+        for(;;){
+            e.setFloor();    
+        }        
+    }
+}
